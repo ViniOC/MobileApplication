76a3c30b141d0c492dcf8317ff616bdd90deba4a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-11 09:57:24 -0300

    aula01

--- Aula01/aula01-intro.js
+++ Aula01/aula01-intro.js
@@ -5,9 +5,66 @@
 // console.log(mensagem("Vinicius"))
 
 
-(function(x,y){
-    console.log(x+y)
-}
-)
+// (function(x,y){
+//     console.log(x+y)
+// }
+// )
 
-(5,7)
+// (5,7)
+
+const triplo = function(num){
+    return num*3
+}
+
+// console.log(triplo(5))
+
+// arrow function -> diminuir e melhorar o desempenho do codigo
+
+let triplov2 = (num) => {
+    return num*3
+}
+
+// console.log(triplov2(3))
+
+// arrow function em uma linha 
+let triplov3 = num  => num*3
+
+// console.log(triplov3(4))
+
+// const rest = (...numeros)=>{
+//     let aux = 0 
+//     for (i in numeros){
+//         aux ++;
+//     }
+//     return aux
+// }
+
+// console.log("REST: ", rest(4,3))
+
+const sum = (...numeros) =>  numeros.reduce((acumulador, x) => acumulador + x, 0);
+
+const sub = (...numeros) =>  numeros.reduce((acumulador, x) => acumulador - x);
+
+const div = (...numeros) =>  numeros.reduce((acumulador, x) => acumulador / x);
+
+const mult = (...numeros) =>  numeros.reduce((acumulador, x) => acumulador * x);
+
+
+
+// console.log(div(2, 2, 3));
+
+
+let x = 9
+let resultado = x%2==0?"par":"impar";
+
+// console.log(resultado);
+
+const pessoa = {
+    nome: "vini",
+    idade: 21,
+    peso: 1
+}
+
+const {nome, idade, peso} = pessoa
+
+console.log(idade);
